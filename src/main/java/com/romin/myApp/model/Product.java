d241d4a888d8ff65ad55011a58043845b9a3bbf1
Romin Khan <rominkhan471@gmail.com>
2026-03-04 04:28:29 +0530

    feat: changes datatype Decimal to BigDecimal

---
+++ src/main/java/com/romin/myApp/model/Product.java
@@ -0,0 +1,29 @@
+package com.romin.myApp.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String discription;
+    private String name;
+    private BigDecimal price;
+    private Integer Quantity;
+}
