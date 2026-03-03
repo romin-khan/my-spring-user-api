9a0634c7cc3a2670f41adb2e167748fc02a0e221
Romin Khan <rominkhan471@gmail.com>
2026-03-03 17:17:36 +0530

    refactor: transform User into a JPA Entity and add JPA dependency

--- src/main/java/com/romin/myApp/model/User.java
+++ src/main/java/com/romin/myApp/model/User.java
@@ -1,5 +1,10 @@
 package com.romin.myApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +12,18 @@ import lombok.NoArgsConstructor;
 @Data // Generates Getters/Setters
 @NoArgsConstructor // Required for Jackson (JSON)
 @AllArgsConstructor // For easy creation
+@Entity
+@Table(name = "Users")
 public class User {
+
+    @Id // This makes 'id' the Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Database will auto-increment the ID
     private Long id;
+
     private String email;
+
     private String name;
+
     private Double contactNo;
+    
 }
